0824129957b53e157b5cf6e3a9ea2e097689e37f
Ruben Arturo Abarca Navarro <70239531+ruge0326@users.noreply.github.com>
2024-12-19 16:43:42 -0600

    chore: stop animation on back

--- app/(tabs)/send.tsx
+++ app/(tabs)/send.tsx
@@ -1,20 +1,25 @@
 import { SkiaCanvasSnowForest } from "@/features/skia_snow_forest/skia_loader";
 import { useLayoutStore } from "@/store/layout";
 import { useFocusEffect } from "expo-router";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 
 export default function SendPage() {
 	const setIsTabBarVisible = useLayoutStore((state) => state.setIsTabBarVisible);
 	const setIsFullScreen = useLayoutStore((state) => state.setIsFullScreen);
+	const [isActive, setIsActive] = useState(false);
 	useFocusEffect(
 		useCallback(() => {
 			setIsTabBarVisible(false);
 			setIsFullScreen(true);
+			setIsActive(true);
 			return () => {
 				setIsTabBarVisible(true);
 				setIsFullScreen(false);
+				setIsActive(false);
 			};
-		}, [setIsTabBarVisible, setIsFullScreen])
+		}, [setIsTabBarVisible, setIsFullScreen, setIsActive])
 	);
+	if (!isActive) return null
 	return <SkiaCanvasSnowForest />;
 }
+
